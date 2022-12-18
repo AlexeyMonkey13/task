f897f2c52bf4ec884137f4ff07af66d065ef4807
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-18 15:53:41 +0700

    наибольшее число из трех

---
+++ secondtask/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите три числа  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number3 < number1 && number1> number2)
+{
+    Console.WriteLine("макс" + number1);
+}
+else 
+if (number2 < number3 && number3 > number1)
+{
+    Console.WriteLine("макс" + number3);
+}
+else
+if (number1 < number2 && number2 > number3)
+{
+    Console.WriteLine("макс" + number2);
+}
