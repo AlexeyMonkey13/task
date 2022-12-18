d7347edcfe115758c7e74e0d12ec1ec45689631d
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-18 16:18:53 +0700

    четное нечетное

---
+++ thirdtak/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0)
+{
+    Console.WriteLine("четное" + number);
+}
+else
+{
+    Console.WriteLine("нечетное " + number);
+}
