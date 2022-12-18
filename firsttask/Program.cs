ca16670c0def3e5f1e810c99c34462335555c829
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-18 13:47:50 +0700

    first task

---
+++ firsttask/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите первое число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write(number1);
+}
+else
+Console.WriteLine("ответ  ");
+{
+    Console.Write(number2);
+}
