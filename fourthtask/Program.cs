840715b3096af7e179c16cfc58bd648b96a49ad1
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-19 12:00:46 +0700

    add fourth task

---
+++ fourthtask/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[number];
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write("четное " + i + " ");
+    }
+}
+
+ 
